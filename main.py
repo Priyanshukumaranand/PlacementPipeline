e5f8ac80cd18fc5f7dddf5c29fa37a5c56d3e4bc
Priyanshu Kumar Anand <118902541+Priyanshukumaranand@users.noreply.github.com>
2025-12-30 17:02:35 +0530

    Configure CORS, remove rate limiting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.api import api_router
 from app.database import engine, Base
 from app.models import Email, PlacementDrive, SyncState
@@ -6,10 +7,24 @@ from app.models import Email, PlacementDrive, SyncState
 # Load environment variables from .env
 from dotenv import load_dotenv
 load_dotenv()
+
 app = FastAPI(
     title="Placement Pipeline",
     description="Automated extraction of placement info from emails",
     version="1.0.0"
+)
+
+origins = [
+    "https://iiit-bbsr-network.vercel.app",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
